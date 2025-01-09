6404f594964e7042d8e2ebcdcba97381d2788388
Ali Mohammadi <ali0182mohammadi@gmail.com>
2025-01-10 02:55:02 +0330

    matrix tricks

--- python-triks/triks.py
+++ python-triks/triks.py
@@ -33,4 +33,63 @@ def training_warlus_2():
 
 # ________________________
 
+li = [1, 2, 2, 3, 9, 7, 8, 3, 3, 8, 8, 8, 8]
+
+
+def most_frequent(list):
+    counter = 0
+    element = list[0]
+    for i in set(list):
+        frequent = list.count(i)
+        if frequent > counter:
+            counter = frequent
+            element = i
+    return element
+
+
+def most_frequent_2(list):
+    print(max(set(list), key=li.count))
+
+
+# ______________________________
+from pprint import pprint
+
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]  
+]
+
+
+def mat_(matrix_):
+    transposed = []
+    for i in range(len(matrix_[0])):
+        transposed_row = []
+        for row in matrix_:
+            transposed_row.append(row[i])
+        transposed.append(transposed_row)
+    pprint(transposed, width=20)
+
+
+def mat_2(matrix_):
+    transposed = []
+    for i in range(len(matrix_[0])):
+        transposed_row = []
+        transposed.append([row[i] for row in matrix_])
+    pprint(transposed, width=20)
+
+
+def mat_3(matrix_):
+    transposed = [[row[i] for row in matrix_] for i in range(len(matrix_[0]))]
+    pprint(transposed, width=20)
+
+
+def mat_4(matrix_):
+    pprint([list(i) for i in list(zip(*matrix_))], width=20)
+
+
+#  __________________________
+
+
 
