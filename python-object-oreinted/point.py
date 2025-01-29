f55e856b2dc272c0ce1495bf8c17adfa135fa73d
Ali Mohammadi <ali0182mohammadi@gmail.com>
2025-01-29 22:08:24 +0330

    class Point

---
+++ python-object-oreinted/point.py
@@ -0,0 +1,56 @@
+from math import hypot
+import doctest
+
+
+class Point:
+    """
+    A Point in the exit provides two-dimensional coordinates.
+    Methots:
+    Atrributes
+    >>> point_1 = Point()
+    >>> point_2 = Point(3, 4)
+    >>> point_1.distance(point_2)
+    5.0
+    """
+
+    def __init__(self, x: float = 0, y: float = 0) -> None:
+        """
+        Initialize x and y coordinates of the new point
+        :param x: x-coordinates
+        :param y: y-coordinates
+        :return: None
+        """
+        self.x = x
+        self.y = y
+        # self.move(x, y)
+
+    def move(self, x: float, y: float) -> None:
+        """
+        Get x and y and move to new position
+        :param x: x-coordination
+        :param y: y-coordination
+        :return: None
+        """
+        self.x = x
+        self.y = y
+
+    def reset(self) -> None:
+        """
+        call Method: move, and go to x=0, y=0 position
+        :return: None
+        """
+        self.move(0, 0)
+
+    def distance(self, other: 'Point') -> float:
+        """
+        get two Point and calculate distance for two-Point
+        :param other: Point object
+        :return: float distance between two-point
+        """
+        return hypot(self.x - other.x, self.y - other.y)
+
+
+# <object>.<attribute> = <value>
+
+
+
